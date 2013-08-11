7ceb3aef82a8f499c8594d087e13fb3919ce6719
jimmytien <jimmytien6222@hotmail.com>
2013-08-12 02:07:16 +0800

    add project
    first version

---
+++ TotalChar/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TotalChar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.infoTotalChar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.bWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(434, 82);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox2_DragDrop);
+            this.listBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox2_DragEnter);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoTotalChar,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 443);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(434, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // infoTotalChar
+            // 
+            this.infoTotalChar.AutoSize = false;
+            this.infoTotalChar.Name = "infoTotalChar";
+            this.infoTotalChar.Size = new System.Drawing.Size(150, 17);
+            this.infoTotalChar.Text = "總字數 :";
+            this.infoTotalChar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 82);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(434, 361);
+            this.splitContainer1.SplitterDistance = 61;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "開始計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "輸出檔案 :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(75, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(347, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(434, 296);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // bWorker1
+            // 
+            this.bWorker1.WorkerReportsProgress = true;
+            this.bWorker1.WorkerSupportsCancellation = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.ClientSize = new System.Drawing.Size(434, 465);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimumSize = new System.Drawing.Size(450, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TotalChar";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel infoTotalChar;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.ComponentModel.BackgroundWorker bWorker1;
+    }
+}
+
---
+++ TotalChar/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TotalChar
+{
+    public partial class Form1 : Form
+    {
+        private System.Collections.Hashtable htable = new System.Collections.Hashtable();
+        
+
+        private void fiFileChar(string fname)
+        {
+            StreamReader inFile = File.OpenText(fname);
+            int linecnt = 0;
+            string outline;
+            while( !inFile.EndOfStream )
+            {
+                string line = inFile.ReadLine();
+                char[] chars = line.ToCharArray();
+                foreach (char c in chars)
+                {
+                    if (!htable.ContainsKey(c))
+                    {
+                        htable[c] = c;
+                        richTextBox1.AppendText(c.ToString());
+                    }
+                }
+            }
+            inFile.Close();
+        }
+
+        private void outputResult(string fname)
+        {
+            StreamWriter outFile = new StreamWriter( fname, false, Encoding.Unicode );
+
+            System.Collections.IDictionaryEnumerator denum = htable.GetEnumerator();
+            int linecnt = 0;
+            while (denum.MoveNext())
+            {
+                char ch = (char)denum.Value;
+                outFile.Write(ch);
+                if (++linecnt == 64)
+                {
+                    outFile.WriteLine();
+                    linecnt = 0;
+                }
+            }
+            outFile.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+               foreach (string file in files)
+                {
+                    listBox2.Items.Add(file);
+                }
+ 
+        }
+
+        private void listBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            // 確定使用者抓進來的是檔案
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                // 允許拖拉動作繼續 (這時滑鼠游標應該會顯示 +)
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            // 確定使用者抓進來的是檔案
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                // 允許拖拉動作繼續 (這時滑鼠游標應該會顯示 +)
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files)
+            {
+                textBox1.Text = file;
+                break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fsInfile;
+            foreach(object listitem in listBox2.Items )
+            {
+                string filename = listitem.ToString();
+                fiFileChar(filename);
+            }
+            outputResult(textBox1.Text);
+        }
+
+    }
+}
